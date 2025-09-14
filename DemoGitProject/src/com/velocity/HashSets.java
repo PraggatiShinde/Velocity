092ed455d20c34578074b914d4e4a575d2256ab0
Pragatti <praggatishinde@gmail.com>
2025-09-14 15:53:46 +0530

    added Integer value in HashSet

---
+++ DemoGitProject/src/com/velocity/HashSets.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class HashSets {
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(10);
+		set.add(20);
+		set.add(40);
+		set.add(60);
+		System.out.println(set);
+	}
+
+}
