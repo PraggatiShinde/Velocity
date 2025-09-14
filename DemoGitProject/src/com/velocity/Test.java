8fa24423b6a658899c2c3d70d73a9f095dcae971
Pragatti <praggatishinde@gmail.com>
2025-09-14 15:48:39 +0530

    added Integer into ArrayList

---
+++ DemoGitProject/src/com/velocity/Test.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+	List<Integer> list = new ArrayList<Integer>();
+	list.add(10);
+	list.add(20);
+	list.add(30);
+	list.add(60);
+	list.add(70);
+	list.add(40);
+	System.out.println(list);
+	}
+
+}
